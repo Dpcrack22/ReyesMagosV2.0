c7e273305045536843a9e0b793618addbf039c51
Dpcrack22 <davidperera2006@gmail.com>
2025-11-07 21:27:11 +0100

    Update HF.js

--- JS/HF.js
+++ JS/HF.js
@@ -1,9 +1,9 @@
 function headerFooter() {
-    fetch('../INCLUDES/header.html')
+    fetch('/INCLUDES/header.html')
         .then(response => response.text())
         .then(data => document.getElementById('header').innerHTML = data);
 
-    fetch('../INCLUDES/footer.html')
+    fetch('/INCLUDES/footer.html')
         .then(response => response.text())
         .then(data => document.getElementById('footer').innerHTML = data);
 };
